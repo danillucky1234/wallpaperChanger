8b3a535c6ebd910d5c582b0690b652ab3d3dd41b
danillucky1234 <danilyurzhenko2607@gmail.com>
2021-07-20 09:18:17 +0300

    Updated changer - from now this changer can set wallpapers at any *nix machine; Added new option - logging; Updated README;

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Installation
-`git clone git@github.com:danillucky1234/wallpaperChanger.git`  
-`cd wallpaperChanger/`  
-`./wallpaperChanger.sh -h`  
+1. `git clone git@github.com:danillucky1234/wallpaperChanger.git`  
+2. `cd wallpaperChanger/`  
+3. `./wallpaperChanger.sh -h`  
 <br>
 
 # Usage
@@ -10,20 +10,26 @@
 Option | Meaning
 -------|-----------
 -h, --help | Show the help message and exit
--t, --time | Set the time after which wallpaper will be updated (default: 30s)
--m, --mixed | Photos will be taken in random order
+-t, --time | Set the time after which wallpaper will be updated [default: 30 seconds]
+-m, --mixed | Photos will be taken in random order [disabled by default]
+-l, --logging | Save all changes to the log file [disabled by default]
 <br>
 
 # Usage examples
-- `./wallpaperChanger.sh ~/Pictures/landscapes/ -t 1m` - This option will change the pictures on the desktop every minute
-- `./wallpaperChanger.sh ~/Pictures/landscapes/ -t "30m 30s"` - This option will change the pictures on the desktop every 30 minutes and 30 seconds
+- `./wallpaperChanger.sh ~/Pictures/wallpapers -t 1m` - This option will change the pictures on the desktop every minute
+- `./wallpaperChanger.sh -t "30m 30s" ~/Pictures/landscapes/` - This option will change the pictures on the desktop every 30 minutes and 30 seconds
 - `./wallpaperChanger.sh ~/Pictures/landscapes/` - This option will change the pictures on the desktop every 30 seconds  
-- `./wallpaperChanger.sh -m ~/Pictures/landscapes/` - This option let you to change the pictures on the desktop every 30 seconds in random order  
+- `./wallpaperChanger.sh -m ~/Pictures/landscapes/ -t 5s -l` - Thsi option will change your wallpapers every 5 seconds in random order and save all changes to the file
+
+## Requirements
+- [feh](https://linux.die.net/man/1/feh)
 
 ## Tip
 I run the script like this:  
-`./wallpaperChanger.sh ~/Pictures/landscapes/ &`
+`./wallpaperChanger.sh <path/to/the/wallpapers> <options> & disown`
 
-I add an '&' at the end to run the script in the background. To terminate the process use:  
+I add an '&' at the end to run the script in the background and remove it from jobs table. It might help you if you accidentally close the terminal.  
+
+To terminate the process use:  
 `ps -aux | grep wallpaper`
 and after this `kill <pid>`
--- wallpaperChanger.sh
+++ wallpaperChanger.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# The program changes the wallpaper on the gnome DE every certain time.
-# It takes as an argument the directory from which it takes pictures and puts them on the desktop
-
 RED='\033[0;31m'
 BLUE='\033[0;36m'
 NC='\033[0m'
 
-if [ $# == 0 ]
-then
+# If user don't passed any arguments to the program - throw exception
+if [ $# == 0 ]; then
 	echo -e "${RED}Not enough arguments. Please specify the path to the directory with the pictures${NC}"
-	exit
+	exit 1
 fi
 
 printHelpMessage ()
@@ -19,59 +16,87 @@ printHelpMessage ()
 	echo ""
 	echo -e "${BLUE}Possible options:${NC}"
 	echo -e "\t-h, --help\t\tPrint this help message"
-	echo -e "\t-t, --time\t\tSet the time after which wallpaper will be updated (default: 30s)"
-	echo -e "\t-m, --mixed\t\tPhotos will be taken in random order"
+	echo -e "\t-t, --time\t\tSet the time after which wallpaper will be updated ${BLUE}[default: 30s]{$NC}"
+	echo -e "\t-m, --mixed\t\tPhotos will be taken in random order ${BLUE}[disabled by default]${NC}"
+	echo -e "\t-l, --logging\t\tLog all wallpaper names with date of usage to log file ${BLUE}[disabled by default]${NC}"
 }
 
-duration="30s" # Duration of the wallpaper
-mixed=false
+# CONFIG VARS
+duration="30s"	# Duration of one picture as wallpaper, 30 seconds by default
+logFile="wallpaperChanger.log"
+
+# Parse arguments
 for ((i=1; i <= $#; ++i))
 do
-	currentNumberOfArgument+=1
-	if [[ "${!i}" == "-h" || "${!i}" == "--help" ]]
-	then
+	if [[ "${!i}" == "-h" || "${!i}" == "--help" ]]; then
 		printHelpMessage
 		exit
-	elif [[ "${!i}" == "-t" || "${!i}" == "--time" ]]
-	then
+	elif [[ "${!i}" == "-t" || "${!i}" == "--time" ]]; then
 		nextArgumentNumber=$((i+1))
 		duration=${!nextArgumentNumber} 
-	elif [[ "${!i}" == "-m" || "${!i}" == "--mixed" ]]
-	then
+		i=$((i+1))
+	elif [[ "${!i}" == "-m" || "${!i}" == "--mixed" ]]; then
 		mixed=true
+	elif [[ "${!i}" == "-l" || "${!i}" == "--logging" ]]; then
+		logging=true
+	else
+		directory=${!i}
 	fi
 done
 
-randomNumber=$((1 + $RANDOM % 10)) # set random number from 1 to 10
-countOfPicturesInTheDirectory=0   # in for loop we will search the count of pictures
-currentNumber=0		   # we will compare this variable with random, if they are same and we use mixed version - we should use this pic, else skip picture
+# Check if direcory exists
+if [[ ! -d $directory ]]; then
+	echo "$directory does not exist or is not a directory"
+    exit 1
+fi
 
+set -e # Bash will throw errors if they occured and stop working
+
+# Add '/' to the end of directory if user doesn't specified it
+if [[ $directory != *"/" ]]; then
+	directory="${directory}/"
+fi
+
+# Fill the array with file names from the directory we want
+iterator=0
+for file in "$directory"*
+do
+	if [[ "$file" == *".png" || "$file" == *".jpg" || "$file" == *".jpeg" ]]; then
+		array[$iterator]=$file
+		iterator=$((iterator+1))
+	fi
+done
+
+iterator=0
+
+# If variable is empty - if statement return false
 while true;
 do
-	# $1 - specified directory
-	for file in "$1"/*
-	do
-		if [[ "$file" == *".png" || "$file" == *".jpg" || "$file" == *".jpeg" ]]
-		then
-			if [ $mixed = true ] # if we use mixed option
-			then
-				if [ $currentNumber == $randomNumber ] # we compare two variables, and if they are same - we change wallpaper
-				then
-					#currentNumber=0 # reset the value
-					gsettings set org.gnome.desktop.background picture-uri "file:///${file}"
-					sleep $duration
-				else # if they aren't same, we increment currentNumber and compare the numbers again
-					currentNumber=$((currentNumber+1))
-				fi
-			else # if we don't use mixed option, we just set new wallpaper every loop cycle
-				gsettings set org.gnome.desktop.background picture-uri "file:///${file}"
-				sleep $duration
+	if [[ $mixed ]]; then
+		# Set random wallpaper
+			file=${array[$RANDOM % ${#array[@]}]}
+			if [[ $logging ]]; then
+				currentTime=$(date +'%Y-%m-%d %H:%M:%S')
+				echo $currentTime >> "$logFile"
+				echo "[$iterator] $file" >> "$logFile"
+				echo "" >> "$logFile"
 			fi
-			countOfPicturesInTheDirectory=$((countOfPicturesInTheDirectory+1))
-		fi
-	done
-	
-	randomNumber=$((1 + $RANDOM % $countOfPicturesInTheDirectory))
-	countOfPicturesInTheDirectory=0
-	currentNumber=0 # reset the value
+			feh --bg-scale $file
+			iterator=$((iterator+1))
+			sleep $duration
+	else
+		# Iterate through the directory and install the wallpaper one by one
+		for file in "${array[@]}"
+		do
+			if [[ $logging ]]; then
+				currentTime=$(date +'%Y-%m-%d %H:%M:%S')
+				echo $currentTime >> "$logFile"
+				echo "[$iterator] $file" >> "$logFile"
+				echo "" >> "$logFile"
+				iterator=$((iterator+1))
+			fi
+			feh --bg-scale $file
+			sleep $duration
+		done
+	fi
 done
